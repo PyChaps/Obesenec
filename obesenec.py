ec49b929ae6256320031aa53b0baac209d7c012e
Kubo <jakub.attackerx@gmail.com>
2020-04-25 15:14:53 +0200

    Initial commit

---
+++ obesenec.py
@@ -0,0 +1,69 @@
+import random
+import os
+
+print("***********************************\n*           *Obesenec*            *\n***********************************\n*coded by: Jakub Krsak, Jan Krsak *\n*version: 1.0.0                   *\n***********************************\n\n\n\n\n\n\n\n")
+
+#sibenica
+sibenice = ["             ", "             ", "             ", "             ", "             ", "             ", "             ", "          _/ \_", 
+"             ", "            |", "            |", "            |", "            |", "            |", "            |", "          _/ \_", 
+"|-----------|", "            |", "            |", "            |", "            |", "            |", "            |", "          _/ \_", 
+"|-----------|", "|           |", "            |", "            |", "            |", "            |", "            |", "          _/ \_", 
+"|-----------|", "|_          |", "( )         |", "            |", "            |", "            |", "            |", "          _/ \_", 
+"|-----------|", "|_          |", "( )         |", " |          |", "            |", "            |", "            |", "          _/ \_", 
+"|-----------|", "|_          |", "( )         |", " |          |", "/|\         |", "            |", "            |", "          _/ \_", 
+"|-----------|", "|_          |", "( )         |", " |          |", "/|\         |", " |          |", "            |", "          _/ \_",
+"|-----------|", "|_          |", "( )         |", " |          |", "/|\         |", " |          |", "/ \         |", "          _/ \_"]
+
+#random word from list
+
+random_word = random.choice(["bobo", "pako", "cvok", "bobk", "gadzo", "macka", "strom", "hrad", "pelikan", "dedoles", "ikebana", "vajicko", "luga", "pipa", "mravciar", "uskatec"])
+random_word_len = len(random_word)
+zoznam_pis = []
+x = 1
+foo = []
+integer1 = 0
+integer2 = 8
+
+zoznam_pis += random_word
+
+for i in range(random_word_len):
+    foo.append("_")
+
+print(foo)
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def sibenica(integer1, integer2):
+    for counter in sibenice[integer1:integer2]:
+        print(counter)
+
+while integer2 <= 72:
+    pismeno = input("pismeno: ")
+    cls()
+    if len(pismeno) == 1:
+        if pismeno in zoznam_pis:
+            print("pismeno sa tam nachadza!!!")
+            index = [z for z, y in enumerate(zoznam_pis) if y == pismeno]
+            #print(index)
+            for item in index: 
+                #int_1 = index.pop(item)
+                foo[item] = pismeno
+            sibenica(integer1, integer2)
+            print(foo)
+            if foo == zoznam_pis:
+                print("UHADOL SI SLOVO:", random_word)
+                quit()
+        else:
+            print("pismeno sa tam nenachadza!!!")
+            sibenica(integer1, integer2)
+            print(foo)
+            integer1 += 8
+            integer2 += 8
+    else:
+        print("Zadaj iba jedno pismeno!!!")
+        x -= 1
+    x += 1
+else:
+    print("NEUHADOL SI SLOVO:", random_word)
+    quit()
